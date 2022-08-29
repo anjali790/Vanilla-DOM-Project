1ff3017f2f22001cf33242eb0043db9de5e808ca
Anjali Yadav <anjaliruby790@gmail.com>
2022-08-30 03:09:02 +0530

    project

--- Reviews/app.js
+++ Reviews/app.js
@@ -5,7 +5,7 @@ const reviews = [
         name: 'Suzain',
         job: 'Web Developer',
         img: 'person1.jpeg',
-        text:  "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+        text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
     },
     {
         id: 2,
@@ -26,7 +26,7 @@ const reviews = [
         name: 'Susan Smith',
         job: 'Designer',
         img: 'person4.jpeg',
-        text:  "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic.",
+        text: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic.",
     },
 
 ];
@@ -55,19 +55,19 @@ window.addEventListener("DOMContentLoaded", function () {
 
 
 //show person based on item:-
-function showPerson(person){
-  const item = reviews[person];
-  img.src = item.img;
-  author.textContent = item.name;
-  job.textContent = item.job;
-  info.textContent = item.text;
+function showPerson(person) {
+    const item = reviews[person];
+    img.src = item.img;
+    author.textContent = item.name;
+    job.textContent = item.job;
+    info.textContent = item.text;
 };
 
 // show next person
 nextBtn.addEventListener("click", function () {
     currentItem++;
     if (currentItem > reviews.length - 1) {
-      currentItem = 0;
+        currentItem = 0;
     }
     showPerson(currentItem);
 });
@@ -76,7 +76,7 @@ nextBtn.addEventListener("click", function () {
 prevBtn.addEventListener("click", function () {
     currentItem--;
     if (currentItem < 0) {
-      currentItem = reviews.length - 1;
+        currentItem = reviews.length - 1;
     }
     showPerson(currentItem);
 });
